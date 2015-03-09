92063af77d64b5ae75213d3d73409861713053f9
Vidur Murali <vidur.murali@gmail.com>
2015-03-09 14:13:50 +0530

    Add OSX aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,8 @@ source $ZSH/.aliases/colors.alias
 source $ZSH/.aliases/git.alias
 source $ZSH/.aliases/navigation.alias
 source $ZSH/.aliases/util.alias
+
+# Add OS specific aliases
+if [[ $(uname -s | grep -i "Darwin") == "Darwin" ]]; then
+    source $ZSH/.aliases/osx.alias
+fi
