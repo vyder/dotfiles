0de22b98eb9b88b962d88531795f85f93d7bcdde
Vidur Murali <vidur.murali@gmail.com>
2016-08-18 12:02:35 +0530

    zsh - 'tree' command accepts a second arg for specific dir

--- files/.zsh/aliases/util.zsh
+++ files/.zsh/aliases/util.zsh
@@ -7,14 +7,20 @@ t () {
                 tree -Ca -L 1 .
         else
                 # If first arg is a number
-        if [ $1 -eq $1 2> /dev/null ]; then
+                if [ $1 -eq $1 2> /dev/null ]; then
                         tree -Ca -L "$1" .
                 # Else if first arg is a directory
                 elif [ -d $1 ]; then
-                        tree -Ca -L 1 "$1"
+
+                        # If second arg is a number
+                        if [ $2 -eq $2 2> /dev/null ]; then
+                                tree -Ca -L "$2" "$1"
+                        else
+                                tree -Ca -L 1 "$1"
+                        fi
                 else # probably passed in custom args
                         tree -Ca "$@"
-        fi
+                fi
         fi
 }
 alias n='nano'
