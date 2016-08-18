d53ed49d016dbf5a0ddc7abdd0137d1f6017227d
Vidur Murali <vidur.murali@gmail.com>
2016-08-18 12:01:54 +0530

    zsh - disable colors shortcuts

--- files/.zsh/aliases/colors.zsh
+++ files/.zsh/aliases/colors.zsh
@@ -1,7 +1,7 @@
 # create color variables
-autoload colors && colors
-for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
-    eval $COLOR='$fg_no_bold[${(L)COLOR}]'  #wrap colours between %{ %} to avoid weird gaps in autocomplete
-    eval BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
-done
-eval RESET='$reset_color'
+# autoload colors && colors
+# for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
+#     eval $COLOR='$fg_no_bold[${(L)COLOR}]'  #wrap colours between %{ %} to avoid weird gaps in autocomplete
+#     eval BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
+# done
+# eval RESET='$reset_color'
