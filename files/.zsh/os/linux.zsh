b2c9dce5bd953acabff0c69b8ad46e7bf3f16fa0
Vidur Murali <vidur.murali@gmail.com>
2025-04-23 18:34:16 +0000

    logs: better journalctl

--- files/.zsh/os/linux.zsh
+++ files/.zsh/os/linux.zsh
@@ -16,3 +16,29 @@ alias 'a'=atom
 alias ss='sudo systemctl'
 compdef ss=systemctl
 
+# Easier journalctl
+unalias logs 2>/dev/null
+
+logs() {
+  sudo journalctl -f "$@" --output=json | jq -r '
+    def color(level):
+      if level == "ERROR" then "\u001b[31m" + level + "\u001b[0m"
+      elif level == "WARN" then "\u001b[33m" + level + "\u001b[0m"
+      else "\u001b[32m" + level + "\u001b[0m"
+      end;
+
+    try (
+      .MESSAGE as $raw
+      | $raw | fromjson
+      | . as $msg
+      | ($msg.level // "INFO" | ascii_upcase) as $lvl
+      | ($msg.identifier // "-") as $ident
+      | $msg.logger as $logger
+      | $msg.msg as $msgtext
+      | (now | strflocaltime("%H:%M:%S")) as $time
+      | "\(color($lvl))  \($time) [\($ident):\($logger)] \($msgtext)"
+    )
+  '
+}
+
+compdef logs=journalctl
